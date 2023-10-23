4c1210fc7e8844844ab3f8741b0baf3011acff79
Oldrado Junior <oldradojunior@gmail.com>
2023-10-23 03:17:00 -0300

    feat: save chosen theme in local storage

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ document.querySelector('#switch button').addEventListener('click', (e) => {
   e.preventDefault();
   const html = document.documentElement;
   html.classList.toggle('dark');
+  const theme = html.classList.contains('dark') ? 'dark' : 'light';
+  localStorage.setItem('theme', theme);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const theme = localStorage.getItem('theme') || 'light';
+  const html = document.documentElement;
+  theme === 'light' ? html.classList.remove('dark') : html.classList.add('dark');
 });
